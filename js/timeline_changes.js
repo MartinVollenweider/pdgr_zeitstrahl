96d015d448f1625b0494621963118d7553feda42
Martin_Vollenweider <martin.vollenweider@htwchur.ch>
2017-02-06 21:34:32 +0100

    V1.1 Daten bis 1949 erfasst; Detailseite eingebaut
    timeline.json beinhaltet Daten bis und mit 1949; details.json einige
    Detaildaten

--- js/timeline_changes.js
+++ js/timeline_changes.js
@@ -14,3 +14,31 @@
         };
 
       	var timeline = new TL.Timeline('timeline-embed','json/timeline.json', options);
+
+// Load details, present in modal window ===========================================
+var jahre;
+$.ajax({
+	type: 'GET',
+	cache: false,
+	url: 'json_details/details.json',
+	dataType: 'json',
+	success: function (daten) {
+		jahre = daten;
+	}
+});	
+
+
+
+// Function triggered by buttons in timeline.json. Parameter is year, eg 1884
+function modalOpen(para) {
+	// Display the modal window
+	$('#m_page').css('display','block');
+
+	// Content dependig on parameter, In detail.json the years are with y_
+	$('#m_html').html(jahre['y_' + para]);
+
+	// When the user clicks on (x), close the modal window
+	$('#m_close').click(function() {
+		$('#m_page').css('display','none');
+	});
+}
